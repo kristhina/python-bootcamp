e7bc869877a19d04fa1bcbd675475e3636638e2c
krysia <krystyna.m.piatkowska@gmail.com>
2017-09-18 14:01:51 +0200

    BlackJack praca nad klasą Hand i Game

--- PycharmProjects/BlackJack/game.py
+++ PycharmProjects/BlackJack/game.py
@@ -53,10 +53,30 @@ class Player:
 
 class Hand:
     def __init__(self):
-        pass
+        self.hand = [self.add_card(), self.add_card()]
+        self.hand_value = self.score_hand()
+
+    def add_card(self):
+        new_card = DeckOfCards.deal_card()
+        return new_card
 
     def score_hand(self):
-        pass
+        hand_value = 0
+        for card in self.hand:
+            if card._card_rank != "A":
+                hand_value += card._card_value[0]
+            else:
+                hand_value += card._card_value[1]
+        #checking if Aces should have value 1 or 11
+        if hand_value > 21:
+            for card in self.hand:
+                if card._card_rank == "A":
+                    hand_value -=10
+                elif hand_value <= 11
+                    hand_value +=10
+        return hand_value
+
+
 
 
 class Game:
@@ -65,6 +85,7 @@ class Game:
         self.dealers_hand = Hand()
         self.player = Player()
         self.stake = stake
+        self.game_deck = DeckOfCards()
 
     def show_menu(self):
         print("This is the BlackJack game")
@@ -77,7 +98,7 @@ class Game:
         while True:
             decision = input("Choose one: a, b, c or d ")
             if decision == "a":
-                pass
+                self.start_game()
             elif decision == "b":
                 self.player.set_name()
             elif decision == "c":
@@ -87,6 +108,81 @@ class Game:
             else:
                 print("You should write: a, b, c or d ")
      #to mi się nie podoba, muszę sobie przemyśleć, jak lepiej ma działać to menu
+
+    def change_stake(self):
+        while True:
+            print("The current stake is {}".format(self.stake))
+            stake_change = input('Do you want to change game stake? "yes" or "no"')
+            if stake_change == "yes":
+                try:
+                    self.stake = int(input("Please write new stake: "))
+                    break
+                except:
+                    print("You have to write a number")
+                    continue
+            elif stake_change == "no":
+                break
+            else:
+                print('Please, write, "yes" or "no"')
+
+    def start_game(self):
+
+        #The player decides about the game stake:
+        self.change_stake()
+
+        #wydrukować kartę dealera
+        print("The dealers opened card is:")
+        # wydrukować kolor i wartość karty dealera
+        print(self.dealers_hand[0]._card_rank, self.dealers_hand[0]._card_suit)
+
+        #początkowa ręka gracza
+        print("{} your cards are:".format(self.player._name))
+        #wydrukować tylko kolor i wartość karty (dlaczego nie używa mi tego carda???)
+        for card in self.players_hand:
+            print(self.players_hand._card_rank, self.players_hand._card_suit)
+
+
+        players_score = self.players_hand.score_hand()
+        print("You have {} points".format(players_score))
+
+        print()
+
+        while True:
+            print('{} do you want to pull another card?'.format(self.player._name))
+            pull = input('"yes" or "no"?')
+            if pull == "yes":
+                self.players_hand.add_card()
+                print(self.players_hand)
+                players_score = self.players_hand.score_hand()
+                print("You have {} points".format(players_score))
+                continue
+            elif pull == "no":
+                break
+            else:
+                print("You didn't write correct answer")
+
+        print("Dealer's cards are:")
+        print(self.dealers_hand)
+        #print dealers score
+        while True:
+            if dealers_score <= 16:
+                self.dealers_hand.append()
+                #score hand
+                #print dealer's score
+                continue
+            else:
+                break
+
+
+
+
+
+
+
+
+        pass
+
+
 
     def finish_game(self):
         exit()
