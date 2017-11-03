7efcf6a3bbf54386f6e3a8aa7c016fa75e82a4ec
krysia <krystyna.m.piatkowska@gmail.com>
2017-11-03 22:05:27 +0100

    review Piontca

--- app1/app1.py
+++ app1/app1.py
@@ -6,25 +6,21 @@ eng_dict = json.load(open("data.json"))
 def translate(word):
     if word in eng_dict:
         return eng_dict[word]
-    elif word.lower() in eng_dict:
+    if word.lower() in eng_dict:
         return eng_dict[word.lower()]
-    elif len(get_close_matches(word, eng_dict.keys())) >0:
+    if len(get_close_matches(word, eng_dict.keys())) > 0:
         print("Did you mean {}?".format(get_close_matches(word, eng_dict.keys())[0]))
         decision = input("y/n")
         if decision == "y":
             return eng_dict[get_close_matches(word, eng_dict.keys())[0]]
-        elif decision == "n":
-            return "so we do not have your word in our dictionary"
-        else:
-            return "you did not write y nor n"
-    else:
-        return "Such word does not exist"
+        if decision == "n":
+            return ["so we do not have your word in our dictionary"]
+        return ["you did not write y nor n"]
+    return ["Such word does not exist"]
 
 word_to_check = input("What do you want to check? ")
 
 output = (translate(word_to_check))
-if type(output) == str:
-    print(output)
-else:
-    for definition in output:
-        print(definition)
+
+for definition in output:
+    print(definition)
