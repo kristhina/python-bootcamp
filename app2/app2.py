8c77e852f717acd28b2628e535371f5d779fd5d6
krysia <krystyna.m.piatkowska@gmail.com>
2017-11-09 15:05:07 +0100

    app2, doesn't work

---
+++ app2/app2.py
@@ -0,0 +1,40 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation <= 1500:
+        return "pink"
+    if 1500 < elevation <= 3000:
+        return "purple"
+    else:
+        return "blue"
+
+map = folium.Map(location=[38.01, -99.09], zoom_start=6, tiles="Mapbox Bright")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+# for lt, ln, el in zip(lat, lon, elev):
+#     fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
+
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=el/300, popup=str(el) + " m", fill = True,
+                                     fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig"),
+                             style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+                                                      else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("Map1.html")
+
+
