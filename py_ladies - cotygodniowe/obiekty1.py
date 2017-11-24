c2146bef7479d10ac0c756e7669de310ba57ae14
krysia <krystyna.m.piatkowska@gmail.com>
2017-11-24 10:42:45 +0100

    c.d. zadania domowego z PyLadies, obiekty 1

--- py_ladies - cotygodniowe/obiekty1.py
+++ py_ladies - cotygodniowe/obiekty1.py
@@ -14,9 +14,9 @@ class Czlowiek:
         return round(self.waga / (self.wzrost * 0.01) ** 2, 2)
 
     def diff_to_norm(self):
-        #niedowaga 18,5
-        #nadwaga 25
-        if self.bmi <18.5:
+        # niedowaga 18,5
+        # nadwaga 25
+        if self.bmi < 18.5:
             oczekiwana_waga = 18.5 * (self.wzrost*0.01) ** 2
             roznica = oczekiwana_waga - self.waga
             print("Musisz przytyć {} kg.".format(roznica))
@@ -31,11 +31,11 @@ class Czlowiek:
 
 
     def save_data(self):
-        with open('{}.json'.format(self.imie), 'w') as file:
+        with open("{}.json".format(self.imie), "w") as file:
             json.dump({"imie": self.imie, "waga": self.waga, "wzrost": self.wzrost}, file)
 
     def to_burn(self):
-        #ile kalorii tracimy przy określonej czynności
+        # ile kalorii tracimy przy określonej czynności
         bieganie = 500
         rower = 600
         hobby = 250
@@ -54,8 +54,8 @@ class Czlowiek:
             print("Żeby schudnąć do dobrej wagi musisz biegać przez {} godzin, jeździć na rowerze przez {} godzin, uprawiać hobby przez {} godzin albo grać w szachy przez {} godzin.".format(godziny_biegania, godziny_roweru, godziny_hobby, godziny_szachy))
 
     def to_eat(self):
-        ziemniaki = 300
-        czekolada = 500
+        ziemniaki = 800     # kalorie w kg
+        czekolada = 4500    # kalorie w kg
 
         oczekiwana_waga = 18.5 * (self.wzrost * 0.01) ** 2
         roznica = oczekiwana_waga - self.waga
@@ -65,15 +65,15 @@ class Czlowiek:
             liczba_kalorii_do_przyjecia = 6000 * roznica
             ziemniaki_do_zjedzenia = round(liczba_kalorii_do_przyjecia/ziemniaki)
             czekolada_do_zjedzenia = round(liczba_kalorii_do_przyjecia/czekolada)
-            print("Żeby przytyć do dobrej wagi musisz zjeść {} ziemniaków albo {} czekolady".format(ziemniaki_do_zjedzenia, czekolada_do_zjedzenia))
+            print("Żeby przytyć do dobrej wagi musisz zjeść {} kg ziemniaków albo {} kg czekolady.".format(ziemniaki_do_zjedzenia, czekolada_do_zjedzenia))
 
 
     def what_to_do(self):
         if self.bmi < 18.5:
-            print("Jesteś za chudy, musisz trochę przytyć")
+            print("Jesteś za chudy, musisz trochę przytyć.")
             self.to_eat()
         elif self.bmi > 25:
-            print("Ważysz za dużo, musisz trochę schudnąć")
+            print("Ważysz za dużo, musisz trochę schudnąć.")
             self.to_burn()
         else:
             print("Ważysz idealnie! Tak trzymaj!")
@@ -91,7 +91,7 @@ class Polityk(Czlowiek):
     def recive_bribe(self):
         self.lapowka = True
 
-pol = Polityk("arnold", 150, 20)
+pol = Polityk("arnold", 150, 35)
 pol.speak()
 pol.recive_bribe()
 pol.speak()
