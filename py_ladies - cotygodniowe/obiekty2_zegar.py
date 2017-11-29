862b55acdabc5f631702d8cfbf0cd9f8613134a5
krysia <krystyna.m.piatkowska@gmail.com>
2017-11-29 10:46:54 +0100

    obiekty2 (zegar) cd.

--- py_ladies - cotygodniowe/obiekty2_zegar.py
+++ py_ladies - cotygodniowe/obiekty2_zegar.py
@@ -12,40 +12,44 @@ class Czas:
             self.g = nowe_godziny
         if nowe_minuty:
             self.m = nowe_minuty
-        self.s = nowe_sekundy or self.s
+        if nowe_sekundy:
+            self.s = nowe_sekundy
+        # self.s = nowe_sekundy or self.s
         # to był trochę inny sposób
 
     def add_time(self, g = None, m = None, s = None):
+        # zgodnie z poleceniem zakładam, że godziny można dodawać w nieskończoność
+        if g:
+            self.g += g
+        if m:
+            self.m += m
+            if self.m >= 60:
+                mtg = self.m // 60
+                self.g += mtg
+                self.m -= mtg*60
+        if s:
+            self.s += s
+            if self.s >= 60:
+                stm = self.s // 60
+                self.m += stm
+                self.s -= stm*60
+                if self.m >= 60:
+                    mtg = self.m // 60
+                    self.g += mtg
+                    self.m -= mtg*60
 
-        # lepiej zacząć od sekund
-        # if g:
-        #     self.g += g
-        # if m:
-        #     self.m += m
-        #     if self.m >= 60:
-        #         mtg = self.m // 60
-        #         self.g += mtg
-        #         self.m -= mtg*60
-        # if s:
-        #     self.s += s
-        #     if self.s >= 60:
-        #         stm = self.s // 60
-        #         self.m += stm
-        #         self.s -= stm*60
-        #         if self.m >= 60:
-        #             mtg = self.m // 60
-        #             self.g += mtg
-        #             self.m -= mtg*60
-        #
 
     def get_seconds(self):
-        pass
+        total_seconds = self.s + self.m*60 + self.g*3600
+        return total_seconds
 
     def get_minutes(self):
-        pass
+        total_minutes = self.s/60 + self.m + self.g * 60
+        return total_minutes
 
     def get_hours(self):
-        pass
+        total_hours = self.s/3600 + self.m/60 + self.g
+        return total_hours
 
 
 class Zegar(Czas):
@@ -65,10 +69,28 @@ class DokladnyZegar(Zegar):
     def __str__(self):
         return super().__str__() + " ms = {}".format(self.ms)
 
-    def set_time(selfself, ms  = None, **kwargs):
+    def set_time(self, ms  = None, **kwargs):
         super().set_time(**kwargs)
         if ms:
             self.ms = ms
+
+    def add_time(self, ms=None, **kwargs):
+        super().add_time(**kwargs)
+        if ms:
+            self.ms += ms
+            if self.ms >= 1000:
+                msts = self.ms // 1000
+                self.s += msts
+                self.ms -= msts*1000
+                if self.s >= 60:
+                    stm = self.s // 60
+                    self.m += stm
+                    self.s -= stm * 60
+                    if self.m >= 60:
+                        mtg = self.m // 60
+                        self.g += mtg
+                        self.m -= mtg * 60
+
 
 def mojprint():
     pass
@@ -80,15 +102,10 @@ print(n_czas.s)
 n_czas.add_time(s = 20)
 print(n_czas)
 
-
-print('-----')
-
 zegar = Zegar(format = '24H', godziny = 12, minuty=34, sekundy=3)
 print(zegar.g)
 zegar.set_time(nowe_godziny=13)
 print(zegar.g)
-
-print('-----')
 
 dz = DokladnyZegar(format = '12H', godziny = 20)
 print(dz.g)
@@ -98,6 +115,14 @@ print(dz.ms)
 print(n_czas)
 print(zegar)
 print(dz)
+
+dz.add_time(ms = 2345, g=17, m=29, s = 78)
+print(dz)
+dz.set_time(ms = 0, nowe_sekundy = 0, nowe_minuty = 10, nowe_godziny=10)
+print(dz)
+print(dz.get_seconds())
+print(dz.get_minutes())
+print(dz.get_hours())
 
 # def __str__(self):
 #     temp = "{} ".format(self._get_name())
