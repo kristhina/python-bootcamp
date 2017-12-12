765fb1dcd14cb5e8219b29b79e9e0076ec3fa5fc
Krystyna Piątkowska <krystyna.m.piatkowska@gmail.com>
2017-12-12 20:13:04 +0100

    html cz.1

---
+++ py_ladies_cotygodniowe/zad3_html.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name")
+    return render_template(
+        'hello.html',
+        naglowek=name,
+    )
+
+app.run(debug=True)
