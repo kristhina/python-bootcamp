443dabbe90afa58d01a6da84bb16095d03696e3f
Krystyna Piątkowska <krystyna.m.piatkowska@gmail.com>
2017-12-12 20:44:28 +0100

    html2

--- py_ladies_cotygodniowe/zad3_html.py
+++ py_ladies_cotygodniowe/zad3_html.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    name = request.args.get("name")
+    name = request.args.get("name", "") # jeśli użytkownik nie poda, to będzie pusty ciąg znaków
     return render_template(
         'hello.html',
         naglowek=name,
     )
 
 app.run(debug=True)
+
+
---
+++ py_ladies_cotygodniowe/zad5_html.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    query = request.form.get('query')
+    return render_template('formularz.html', query=query)
+
+app.run(debug=True)
