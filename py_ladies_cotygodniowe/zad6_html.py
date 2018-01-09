13b49949c10a72bd9620a1bd5c2ad74f6c2a6381
krysia <krystyna.m.piatkowska@gmail.com>
2018-01-09 13:32:29 +0100

    zad6 html

--- py_ladies_cotygodniowe/zad6_html.py
+++ py_ladies_cotygodniowe/zad6_html.py
@@ -7,10 +7,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/add_form", methods = ["GET", "POST"])
+
+@app.route("/add_form", methods=["GET", "POST"])
 def add():
-    liczba1 = request.form.get('liczba1')
-    liczba2 = request.form.get('liczba2')
-    return render_template('add_form.html', liczba1=liczba1, liczba2=liczba2)
+    try:
+        liczba1 = request.form.get('liczba1')
+        liczba2 = request.form.get('liczba2')
+        if liczba1 or liczba2 is not None:
+            liczba1 = int(liczba1)
+            liczba2 = int(liczba2)
+            my_sum = liczba1 + liczba2
+            return render_template('add_form.html', result=my_sum)
+        else:
+            return render_template("add_form.html")
+    except ValueError:
+        error = True
+        return render_template("add_form.html", error=error)
+
 
 app.run(debug=True)
