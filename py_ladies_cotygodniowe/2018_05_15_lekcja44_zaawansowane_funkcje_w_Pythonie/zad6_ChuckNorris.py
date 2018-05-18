d01f9573b7f99bb8c4ae3137df77bc4f79fb2ba8
krysia <krystyna.m.piatkowska@gmail.com>
2018-05-18 12:45:06 +0200

    zaawansowane funkcje w Pythonie - zadanie domowe

--- py_ladies_cotygodniowe/2018_05_15_lekcja44_zaawansowane_funkcje_w_Pythonie/zad6_ChuckNorris.py
+++ py_ladies_cotygodniowe/2018_05_15_lekcja44_zaawansowane_funkcje_w_Pythonie/zad6_ChuckNorris.py
@@ -4,19 +4,23 @@
 
 import requests
 import json
-#
-# chn = requests.get("https://api.icndb.com/jokes/1")
-# jokes = json.loads(chn.content)
-# print(jokes['value']['joke'])
+
 
 def chuck_norris():
     i = 1
-    while True:
+    missed = 0
+    while missed < 20:
         chn = requests.get("https://api.icndb.com/jokes/{}".format(str(i)))
         joke_content = json.loads(chn.content)
-        joke = joke_content['value']['joke']
-        yield joke
-        i += 1
+        try:
+            joke = joke_content['value']['joke']
+            yield joke
+            i += 1
+            missed = 0
+        except TypeError:
+            i += 1
+            missed += 1
+
 
 for joke in chuck_norris():
     print(joke)
--- py_ladies_cotygodniowe/2018_05_15_lekcja44_zaawansowane_funkcje_w_Pythonie/zad7_ChuckNorris_with_namedtuple.py
+++ py_ladies_cotygodniowe/2018_05_15_lekcja44_zaawansowane_funkcje_w_Pythonie/zad7_ChuckNorris_with_namedtuple.py
@@ -1,27 +1,43 @@
 # Na rozszerz generator z zad 6. tak, żeby zwracał instancję klasy lub namedtuple zwierającą
 # dowcip, id, czas stworzenia klasy, czas trwania zapytania.
 
+import requests
+import json
 from datetime import datetime
 
-class Zart:
-    def __init__(self, id, zart, czas_st, delta_t):
+
+class Joke:
+    def __init__(self, id, joke, time, delta_t):
         self.id = id
-        self.zart = zart
-        self.czas_st = czas_st
+        self.joke = joke
+        self.time = time
         self.delta_t = delta_t
 
+    def __repr__(self):
+        return "Dowcip o numerze {} został wygenerowany {}. " \
+               "Czas trwania zapytania wyniósł {}, a jego treść to: {}"\
+            .format(self.id, self.time, self.delta_t, self.joke)
 
-def generatorek()
+
+def chuck_norris():
     i = 1
     missed = 0
-    while True:
+    while missed < 20:
         start = datetime.now()
-        resp = request.get()
+        chn = requests.get("https://api.icndb.com/jokes/{}".format(str(i)))
         end = datetime.now()
         delta = end - start
-        if resp["type"] == "success":
-            yield resp
+        joke_content = json.loads(chn.content)
+        try:
+            chn_joke = joke_content['value']['joke']
+            joke = Joke(i, chn_joke, start, delta)
+            yield joke
+            i += 1
             missed = 0
-        else missed +=1
-        if missed > 10:
-            break
+        except TypeError:
+            i += 1
+            missed += 1
+
+
+for joke in chuck_norris():
+    print(joke)
